9cbe5444f66612f3170652690df4352956af78f5
Hunter <haeilmes@gmail.com>
2019-08-30 08:26:13 -0500

    Intial commit for assignment

---
+++ MethodsAndClasses/MethodsAndClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
